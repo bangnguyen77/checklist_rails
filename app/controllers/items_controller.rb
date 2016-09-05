e2ad2dbb8618e4adc4ea6a3ef471b0f6c640a5ce
Bang Nguyen <bangnv@gmail.com>
2016-09-05 16:22:09 -0700

    add edit functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
   private
     def item_params
       params.require(:item).permit(:title, :description)
