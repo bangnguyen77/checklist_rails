8bf003153c0379a2c6e656dabe7a15c5a9736004
Bang Nguyen <bangnv@gmail.com>
2016-09-05 18:31:49 -0700

    add some styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items do
     member do
-      patch :complete
+      patch :complete #related to method defined in items_controller
     end
   end
   root 'items#index'
